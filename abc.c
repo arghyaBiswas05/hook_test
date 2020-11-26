a2c785387a83c5df3492a945aeec352256f67238
Arghya Biswas <arghya@ialoy.com>
2020-11-26 21:42:31 +0530

    finally its looks like that it is wokring....
    Signed-off-by: Arghya Biswas <arghya@ialoy.com>

---
+++ abc.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2015-2017, ARM Limited and Contributors. All rights reserved.
+
+#include <stdio.h>
+
+int main(void)
+
+{
+	printf("wokring\n");
+	return 0;
+}
